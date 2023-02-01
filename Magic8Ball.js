ffaf6afc99980ba1b5d45f4f998a12a67affbb38
Eric Troutman <erictroutman2012@gmail.com>
2023-01-31 19:01:41 -0500

    First Commit

---
+++ Magic8Ball.js
@@ -0,0 +1,23 @@
+const userName = 'Eric';
+userName ? console.log(`Hello, ${userName}!`) : console.log(`Hello!`);
+let userQuestion = 'enter question here?';
+console.log(`${userName} asked, ${userQuestion}`);
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = ''
+switch(randomNumber) {
+case 0: console.log('It is certain')
+break;
+case 1: console.log('It is decidedly so')
+break;
+case 2: console.log('Reply hazy try again')
+break;
+case 3: console.log('Cannot predict now')
+break;
+case 4: console.log('Do not count on it')
+break;
+case 5: console.log('My sources say no')
+break;
+case 6: console.log('Outlook not so good')
+break;
+case 7: console.log('Signs point to yes')
+break; }
